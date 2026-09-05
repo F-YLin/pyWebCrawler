6910e1dff52306198a21e5e66cf0ede95b878830
F-YLin <linnx@engineering.upenn.edu>
2026-09-04 17:48:42 -0700

    feat(cli): add command-line argument validation and entrypoint. update README.md
    - Parse CLI arguments using `sys.argv`
    - Validate that exactly one argument (the base URL) is provided
    - Exit with status code 1 and display an error message if too few or too many arguments are passed
    - Print confirmation message indicating crawl start at the target URL

---
+++ README.md
@@ -0,0 +1,3 @@
+The program takes as a command line argument the website URL we want to crawl by running
+uv run main.py BASE_URL
+where BASE_URL is the root URL of the website to crawl
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from pywebscraper!")
+import sys
 
+def main():
+    # # uv run example.py -v
+    # print("Script name:", sys.argv[0])  # example.py
+    # print("Argument:", sys.argv[1])     # -v
+
+    # if the number of CLI arguments is less than 2
+    # print error message and exit with code 1
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+
+    # if the number of CLI arguments is more than 2
+    # print error message and exit with code 1
+    if len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+        
+    else:
+        print(f"starting crawl of: {sys.argv[1]}")
 
 if __name__ == "__main__":
     main()
